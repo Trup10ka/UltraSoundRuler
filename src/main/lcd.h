3836ea510b9aa2f8175d8045639f71c528bc3d3f
Trup10ka <lukys3036@gmail.com>
2024-03-04 23:14:27 +0100

    Implement 'lcd.h'

---
+++ src/main/lcd.h
@@ -0,0 +1,15 @@
+#ifndef LCD_SCAN_H
+
+/* This condition is needed for the compiler to know that any source code that includes this header, is a C code, thus adjusting the compilation process */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Varies from users device; scan for the address is needed; you can find the scanner script in this project in the 'lcd_addr_scan' folder */
+#define LCD_MEM_ADDRESS 0x3F
+
+#ifdef __cplusplus
+}
+#endif
+
+#endifd
