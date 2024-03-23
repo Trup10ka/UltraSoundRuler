b69e3b249f8ff8cd352a6c6ae359c36aece9c3b7
Trup10ka <lukys3036@gmail.com>
2024-03-23 15:00:25 +0100

    Add configuration for ultrasound pins

--- src/main/pins.c
+++ src/main/pins.c
@@ -7,7 +7,14 @@ static void config_system_led()
     digitalWrite(SYSTEM_LED, HIGH);
 }
 
+static void config_ultrasound_sensor_pins()
+{
+    pinMode(PIN_D0, INPUT);
+    pinMode(PIN_D1, OUTPUT);
+}
+
 void setup_pins()
 {
     config_system_led();
+    config_ultrasound_sensor_pins();
 }
