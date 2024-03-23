a2764bdf3bda5bfe29c830c30111da8f7bc91b2b
Trup10ka <lukys3036@gmail.com>
2024-03-23 15:03:16 +0100

    Implement testing loop for measuring distance

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,14 +1,23 @@
 #include "pins.h"
+#include "ultrasound_dev.h"
+#include <LiquidCrystal_I2C.h>
+
+ParseType speedOfLightInCm = { "CM", SPEED_OF_SOUND_IN_CM };
+ParseType speedOfLightInInch = { "INCH", SPEED_OF_SOUND_IN_INCH };
 
 void setup() 
 {
+    Serial.begin(9600);
     setup_pins();
 }
-
 void loop() 
 {
-    digitalWrite(SYSTEM_LED, LOW);
-    delay(1000);
-    digitalWrite(SYSTEM_LED, HIGH);
-    delay(4000);
+    trigger_measure();
+    float distance = parse_distance(speedOfLightInInch);
+
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(speedOfLightInInch.name);
+
+    delay(1500);
 }
