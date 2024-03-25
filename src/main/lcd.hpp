add75f5e2cf0b1f754dd8cceb8b487708ce9686a
Trup10ka <lukys3036@gmail.com>
2024-03-25 21:13:03 +0100

    Declare lcd header

---
+++ src/main/lcd.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+/* Varies from users device; scan for the address is needed; you can find the scanner script in this project in the 'lcd_addr_scan' folder */
+#define LCD_MEM_ADDRESS 0x3F
+
+/* This might vary based on your LCD, but for most of the cases this is valid */
+#define LCD_ROWS 2
+#define LCD_COLUMNS 16
+
+void init_lcd();
+
+void print_distance(float distance);
