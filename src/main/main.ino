6508ee7e05d8450d6bffda640e394960b338f36f
Trup10ka <lukys3036@gmail.com>
2024-03-27 18:46:23 +0100

    Update main loop and setup methods, remove serial printout

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,23 +1,21 @@
 #include "pins.h"
 #include "ultrasound_dev.h"
-#include <LiquidCrystal_I2C.h>
+#include "lcd.hpp"
 
-ParseType speedOfLightInCm = { "CM", SPEED_OF_SOUND_IN_CM };
-ParseType speedOfLightInInch = { "INCH", SPEED_OF_SOUND_IN_INCH };
+ParseType speedOfLightInCm = { " CM", SPEED_OF_SOUND_IN_CM };
+ParseType speedOfLightInInch = { " INCH", SPEED_OF_SOUND_IN_INCH };
 
 void setup() 
 {
-    Serial.begin(9600);
     setup_pins();
+    init_lcd();
 }
 void loop() 
 {
     trigger_measure();
     float distance = parse_distance(speedOfLightInInch);
 
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.println(speedOfLightInInch.name);
+    print_distance(distance, speedOfLightInInch.name);
 
-    delay(1500);
+    delay(500);
 }
